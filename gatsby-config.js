03b0507fbaf5a7ea716d2b1c05fec0ef6bc36496
Cpt_Crunch <jasong84@gmail.com>
2020-02-26 21:19:14 -0600

    Edit gatsby-config to match organization contact information

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,11 @@ const guid = process.env.NETLIFY_GOOGLE_ANALYTICS_ID;
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Serif',
-    description: 'my theme',
+    title: 'Alamo Post 2 | American Legion',
+    description: 'Non profit veteran organization',
     contact: {
-      phone: 'XXX XXX XXX',
-      email: 'zerostaticthemes@gmail.com',
+      phone: '210 732 1891',
+      email: 'info@alamopost2.org',
     },
     menuLinks: [
       {
