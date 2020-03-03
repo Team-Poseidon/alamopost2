f1690eb6c55c4c137c58e4466c37a6f14f6a26ac
Cpt_Crunch <jasong84@gmail.com>
2020-03-03 10:10:37 -0600

    Add Tachyons to site and edit card layout

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -8,12 +8,16 @@ const SEO = (props) => ( <
     render = {
         (data) => {
             const title = props.title || data.site.siteMetadata.title;
-            return ( <Helmet htmlAttributes = {{lang: 'en',}}
+            return ( 
+                <Helmet htmlAttributes = {{lang: 'en',}}
                 title = { title }
                 titleTemplate = { `%s - ${data.site.siteMetadata.title}` }
                 link = {
                     [{ rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }] }
-                />
+                    >
+
+                    <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
+                </Helmet>
             );
         }
     }
