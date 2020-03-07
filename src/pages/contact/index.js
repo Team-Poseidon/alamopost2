e8fa2be415ee68d494796f229af2776356b9d932
Jason Gutierrez <jasong84@gmail.com>
2020-03-06 18:02:41 -0600

    Add placeholder image for post on contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -7,15 +7,19 @@ import BusinessHours from '../../components/BusinessHours';
 const Contact = props => (
   <Layout bodyClass="page-contact">
     <SEO title="Contact" />
-    <div className="intro intro-small">
+    <div className="intro">
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h1>Contact</h1>
+            <h1>Contact Alamo Post 2</h1>
+            <div className="pa1 pa1-ns">
+                <img src="http://tachyons.io/img/space.jpg" className="w-100 f5 measure" alt="Photo of outer space" />
+            </div>
           </div>
         </div>
       </div>
     </div>
+    
     <div className="container">
       <div className="row">
         <div className="col-12">
