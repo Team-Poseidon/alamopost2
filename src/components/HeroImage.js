bcc9944d7514b55ed13c22f1ac059c10ed7d8773
Cpt_Crunch <jasong84@gmail.com>
2020-03-07 00:07:36 -0600

    Make HeroImage component  consume props

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import banner from '../../static/images/legion_family_emblem.jpg';
+
 
 const HeroImage = props => (
     <React.Fragment>
         <div className="pa1 tc">
-            <img src={banner} className="w-100 dib measure f3" alt="Photo of outer space" />
+            <img src={props.source} className="w-100 dib measure f3" alt={props.altName} />
         </div>
     </React.Fragment>
 )
