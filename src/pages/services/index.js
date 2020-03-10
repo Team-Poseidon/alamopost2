d34d8043d5f83609fb077d92e7365d476c58f04b
Jason Gutierrez <jasong84@gmail.com>
2020-03-10 17:09:59 -0500

    Use services component on Services page and deleted entry

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
 import SEO from '../../components/SEO';
 import Layout from '../../layouts/index';
+import Services from '../../components/Services';
 
-const Services = (props) => {
-  const services = props.data.allMarkdownRemark.edges;
-  return (
+const ServicesPage = (props) => {
+ return (
     <Layout bodyClass="page-services">
       <SEO title="Services" />
       <div className="intro">
         <div className="container">
           <div className="row">
             <div className="col-12">
-              <h1>Services</h1>
+              <h1>Post Services</h1>
             </div>
           </div>
         </div>
@@ -20,41 +19,12 @@ const Services = (props) => {
 
       <div className="container pb-6">
         <div className="row">
-          {services.map(edge => (
-            <div key={edge.node.frontmatter.path} className="col-12 col-md-4 mb-1">
-              <div className="card service service-teaser">
-                <div className="card-content">
-                  <h2>
-                    <Link to={edge.node.frontmatter.path}>{edge.node.frontmatter.title}</Link>
-                  </h2>
-                  <p>{edge.node.excerpt}</p>
-                </div>
-              </div>
-            </div>
-          ))}
+          <Services />
         </div>
       </div>
     </Layout>
   );
 };
 
-export const query = graphql`
-  query ServicesQuery {
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/services/" } }
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      edges {
-        node {
-          excerpt
-          frontmatter {
-            title
-            path
-          }
-        }
-      }
-    }
-  }
-`;
 
-export default Services;
+export default ServicesPage;
